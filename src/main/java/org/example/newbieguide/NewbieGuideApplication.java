57e1f973ad8d9e966b173d25c3f7e3f17101994c
vamlin <lsc991123@gmail.com>
2024-06-23 21:25:11 +0900

    init

---
+++ src/main/java/org/example/newbieguide/NewbieGuideApplication.java
@@ -0,0 +1,13 @@
+package org.example.newbieguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbieGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbieGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newbieguide/NewbieGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newbieguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbieGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
